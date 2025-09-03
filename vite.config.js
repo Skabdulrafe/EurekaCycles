a3975c616e2f8bde9dc1b9df08f9e4d2639b8c1d
shaikh rafe <skabdulrafe75@gmail.com>
2025-09-04 01:44:12 +0530

    oone

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// })
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
   ],
-  build: {
-    outDir: 'dist', // Vercel expects this (default for Vite)
-  },
-  server: {
-    port: 3000, // local dev only
-  },
-  base:process.env.VITE_BASE_PATH||"EurekaCycles",
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   build: {
+//     outDir: 'dist', // Vercel expects this (default for Vite)
+//   },
+//   server: {
+//     port: 3000, // local dev only
+//   },
+//   base:process.env.VITE_BASE_PATH||"EurekaCycles",
+// })
