9ff8326b53cf293e24bcaccd629cf9407d34df2e
shaikh rafe <skabdulrafe75@gmail.com>
2025-09-03 16:04:11 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist', // Vercel expects this (default for Vite)
+  },
+  server: {
+    port: 3000, // local dev only
+  }
 })
